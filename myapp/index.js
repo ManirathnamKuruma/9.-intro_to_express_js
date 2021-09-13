8fa9d522818b9a30f8ea3b7f7f94841ffbc6dbd8
Mani Kuruma <kuruma.manirathnam@gmail.com>
2021-09-13 11:12:57 +0530

    first commit

---
+++ myapp/index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+
+app.get("/",(request,response)=>{
+    response.send("Hello Mani Kuruma!")
+});
+
+app.get("/date",(request,response)=>{
+    let date=new Date();
+    response.send(`Today's date is ${date}`);
+})
+
+app.listen(4000);
